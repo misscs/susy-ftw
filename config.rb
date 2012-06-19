dab63bb321e48b5963370bce590574eb9e53cc2a
cs <claudina.sarahe@gmail.com>
2012-06-19 14:12:43 -0400

    html5 boilerplate and now a compass project

---
+++ config.rb
@@ -0,0 +1,36 @@
+# Require any additional compass plugins here.
+require "susy"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+ output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+ relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+ preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# Change this to :production when ready to deploy the CSS to the live server.
+#environment = :development
+environment = :production
+
+# Pass options to sass.
+# - For development, we turn on the FireSass-compatible debug_info.
+# - For production, we force the CSS to be regenerated even though the source
+#   scss may not have changed, since we want the CSS to be compressed and have
+#   the debug info removed.
+sass_options = (environment == :development) ? {:debug_info => true} : {:always_update => true}
